5ddbf1aa07dcb0041467a437d75344dd3e32dccb
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-03-19 21:02:29 +0200

    Add project files.

---
+++ MatchPhoneNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+            string pattern = @"\+359( |-)[2]\1\d{3}\1\d{4}\b";
+
+            MatchCollection numbers = Regex.Matches(input, pattern);
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
